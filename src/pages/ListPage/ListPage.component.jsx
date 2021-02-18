937f46318053392415a697ae5b8ab18e8c4c1f92
mattia <mattiav.dev@gmail.com>
2021-02-18 17:53:21 +0200

    Test enable VS code to create commit messages

--- src/pages/ListPage/ListPage.component.jsx
+++ src/pages/ListPage/ListPage.component.jsx
@@ -4,7 +4,7 @@ import MoviePopular2 from '../../components/MoviePopular/MoviePopular2.component
 import {getMoviePopular} from '../../utils/utilities';
 
 const ListPage = (props) => {
-  //based on the props.match.path variable we will load the appropriate component (with the approrpiete API query)
+  //based on the props.match.url variable we will load the appropriate component (with the approrpiete API query)
   console.log(props);
   const url = props.match.url;
   console.log("URL:", url);
@@ -16,7 +16,7 @@ const ListPage = (props) => {
       break;
   
     default:
-      ListComponent = MoviePopular2;
+      ListComponent = MoviePopular2; //test 
       break;
   }
 
